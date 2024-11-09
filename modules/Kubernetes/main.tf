acd9ac778fe113b48291972de85460fe0140b97b
apulido <apulidoc22@gmail.com>
2024-11-09 12:26:48 -0500

    Add variables

--- modules/Kubernetes/main.tf
+++ modules/Kubernetes/main.tf
@@ -7,6 +7,7 @@ resource "google_container_cluster" "primary" {
 
   node_config {
     machine_type = var.machine_type
+    disk_size_gb = 50
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring"
