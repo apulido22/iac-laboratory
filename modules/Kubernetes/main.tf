284600a0eb7eb3b2dbfe6c0ad759c96fc031a19d
apulido <apulidoc22@gmail.com>
2024-11-11 20:52:45 -0500

    Feat: Agregar creación de Artifact

--- modules/Kubernetes/main.tf
+++ modules/Kubernetes/main.tf
@@ -16,11 +16,3 @@ resource "google_container_cluster" "primary" {
     ]
   }
 }
-
-provider "kubernetes" {
-  host                   = google_container_cluster.primary.endpoint
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.cluster_ca_certificate)
-}
-
-data "google_client_config" "default" {}
