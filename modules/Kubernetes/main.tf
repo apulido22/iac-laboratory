24dd4f575fddc79b8b2f1595b10c34df1d097a92
apulido <apulidoc22@gmail.com>
2024-11-12 22:18:06 -0500

    Feat: Agregar creación de servicios y pods

--- modules/Kubernetes/main.tf
+++ modules/Kubernetes/main.tf
@@ -12,7 +12,8 @@ resource "google_container_cluster" "primary" {
     disk_size_gb = 50
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring"
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.read_only"
     ]
   }
 }
