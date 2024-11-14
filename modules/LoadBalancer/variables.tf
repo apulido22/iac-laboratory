d26bca89ae7f089036f6e4484303e5c75a587b6b
apulido <apulidoc22@gmail.com>
2024-11-13 22:33:18 -0500

    Feat: Agregar Functions

--- modules/LoadBalancer/variables.tf
+++ modules/LoadBalancer/variables.tf
@@ -4,3 +4,8 @@ variable "project_id" {
   description = "ID del proyecto de GCP"
   type        = string
 }
+
+variable "storage_bucket_name" {
+  type = string
+  description = "Nombre del bucket de almacenamiento para el sitio web"
+}
