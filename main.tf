1a84972b3acf6bf157ba23b24a72188715e09334
apulido <apulidoc22@gmail.com>
2024-11-13 22:58:48 -0500

    Excluyendo .terraform de git tracking

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ module "website_loadbalancer" {
   source     = "./modules/LoadBalancer"
   project_id = var.project_id
   depends_on = [module.website_storage]
-  bucket_name = module.website_storage.website_bucket_name
+  storage_bucket_name = module.website_storage.website_bucket_name
 }
 
 module "cloud_function" {
